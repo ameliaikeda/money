65b87f109d68dfd6952462a8dc153828204ae39f
Amelia Ikeda <amelia@dorks.io>
2015-08-28 11:54:51 +0100

    Add tests for the OpenExchangeRates API class against a mock API

---
+++ test/Api/Adapter/MockOpenExchangeRatesClient.php
@@ -0,0 +1,81 @@
+<?php namespace Amelia\Money\Api\Adapter;
+
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\RequestInterface;
+
+class MockOpenExchangeRatesClient implements AdapterInterface
+{
+    /**
+     * Overridden URL for tests
+     *
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * If we send json or not
+     *
+     * @var bool
+     */
+    protected $bad = false;
+
+    /**
+     * Test-specific helper for testing our array responses
+     *
+     * @param string $url
+     */
+    public function setEndpoint($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get the filename of our sample data
+     *
+     * @param string $url
+     * @return string
+     */
+    protected function getEndpoint($url)
+    {
+        return __DIR__ . '/data' . ($this->url ?: $url);
+    }
+
+    /**
+     * Simple helper method to create a request for a GET and send it
+     *
+     * @param string $url The URL to fetch
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function fetch($url)
+    {
+        $body = file_get_contents($this->getEndpoint($url));
+        $headers = ['Content-Type' => ($this->bad) ? 'text/html' : 'application/json'];
+
+        return new Response(
+            array_get(json_decode($body, true), "status", 200), // status code will override whatever is sent
+            $headers,
+            $body
+        );
+    }
+
+    /**
+     * Request directly using a PSR-7 request object
+     *
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function request(RequestInterface $request)
+    {
+        return;
+    }
+
+    /**
+     * Do we return a malformed json response?
+     *
+     * @param bool $bad
+     */
+    public function setBadRequest($bad)
+    {
+        $this->bad = $bad;
+    }
+}
---
+++ test/Api/OpenExchangeRatesTest.php
@@ -0,0 +1,198 @@
+<?php namespace Amelia\Money\Api;
+
+use Amelia\Money\Api\Adapter\MockOpenExchangeRatesClient;
+use Carbon\Carbon;
+use PHPUnit_Framework_TestCase;
+
+class OpenExchangeRatesTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Amelia\Money\Api\Adapter\MockOpenExchangeRatesClient
+     */
+    protected $client;
+
+    /**
+     * Set up tests
+     */
+    public function setUp() {
+        $this->client = new MockOpenExchangeRatesClient();
+    }
+
+    public function testInitializesCorrectly()
+    {
+        $api = new OpenExchangeRates($this->client);
+        $this->assertInstanceOf('Amelia\Money\Api\OpenExchangeRates', $api);
+    }
+
+    public function testFetchLatest()
+    {
+        $api = new OpenExchangeRates($this->client);
+        $latest = $api->getLatest();
+
+        $this->assertInstanceOf('Amelia\Money\Container\RateContainerInterface', $latest);
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\InvalidResponseException
+     * @expectedExceptionMessage json was expected
+     */
+    public function testFetchLatestBadRequest()
+    {
+        $this->client->setBadRequest(true);
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\InvalidResponseException
+     * @expectedExceptionMessage NULL
+     */
+    public function testNullResponse()
+    {
+        $this->client->setEndpoint('/errors/null.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\AccessDeniedException
+     * @expectedExceptionMessage access has been revoked
+     * @expectedExceptionCode 403
+     */
+    public function testAccessRestrictedError()
+    {
+        $this->client->setEndpoint('/errors/access-restricted.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\AccessDeniedException
+     * @expectedExceptionMessage only available for Enterprise and Unlimited clients
+     * @expectedExceptionCode 403
+     */
+    public function testNotAllowedError()
+    {
+        $this->client->setEndpoint('/errors/not-allowed.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\InvalidAuthException
+     * @expectedExceptionMessage Invalid App ID
+     * @expectedExceptionCode 400
+     */
+    public function testInvalidAppIdError()
+    {
+        $this->client->setEndpoint('/errors/invalid-app-id.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\InvalidAuthException
+     * @expectedExceptionMessage Missing App ID
+     * @expectedExceptionCode 400
+     */
+    public function testMissingAppIdError()
+    {
+        $this->client->setEndpoint('/errors/missing-app-id.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\BaseNotFoundException
+     * @expectedExceptionMessage unsupported `base` currency
+     * @expectedExceptionCode 400
+     */
+    public function testInvalidBaseError()
+    {
+        $this->client->setEndpoint('/errors/invalid-base.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\NotFoundException
+     * @expectedExceptionMessage Historical rates for the requested date are not available
+     * @expectedExceptionCode 400
+     */
+    public function testNotAvailableError()
+    {
+        $this->client->setEndpoint('/errors/not-available.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\NotFoundException
+     * @expectedExceptionCode 404
+     */
+    public function testNotFoundError()
+    {
+        $this->client->setEndpoint('/errors/not-found.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\RateLimitException
+     * @expectedExceptionMessage Too Many Requests from your App Key
+     * @expectedExceptionCode 429
+     */
+    public function testTooManyRequestsError()
+    {
+        $this->client->setEndpoint('/errors/too-many-requests.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\InvalidResponseException
+     * @expectedExceptionMessage an internal error occurred (internal_error)
+     * @expectedExceptionCode 500
+     */
+    public function testGenericError()
+    {
+        $this->client->setEndpoint('/errors/generic.json');
+        $api = new OpenExchangeRates($this->client);
+        $api->getLatest();
+    }
+
+    public function testGetHistoricalRates()
+    {
+        $api = new OpenExchangeRates($this->client);
+        $rates = $api->getHistorical(Carbon::parse('2011-10-18'));
+
+        $this->assertInstanceOf('Amelia\Money\Container\RateContainerInterface', $rates);
+    }
+
+    /**
+     * @expectedException \Amelia\Money\Exception\NotFoundException
+     * @expectedExceptionMessage Historical rates for the requested date are not available
+     * @expectedExceptionCode 400
+     */
+    public function testGetOldHistoricalRates()
+    {
+        $api = new OpenExchangeRates($this->client);
+        $api->getHistorical(Carbon::parse('1998-01-22'));
+    }
+
+    /**
+     * We don't have any way to fetch updates from a certain point
+     *
+     * @expectedException \Amelia\Money\Exception\NotFoundException
+     */
+    public function testGetUpdates()
+    {
+        $api = new OpenExchangeRates($this->client);
+        $api->getUpdates(Carbon::now());
+    }
+
+
+
+
+
+}
